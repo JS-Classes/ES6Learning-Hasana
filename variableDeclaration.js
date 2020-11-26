9f37852242f0dd291f4c2d5d2ac5a3e00e823af1
Hasana Shrestha <i157884@d2hs.com>
2020-11-26 09:29:21 +0545

    variable_leaning

--- variableDeclaration.js
+++ variableDeclaration.js
@@ -3,6 +3,33 @@ var g = ["Apple", "Orange"]
 var f = "Apple";
 var aj = {name: "Hasana",  gender: "female"}
 var num = 2 ** 2;
+// Primitive
+var a = "bishnu";
+var b = true;
+
+var c = 345.6;
+var c1 = +Infinity;
+var c2 = -Infinity;
+var c3 = NaN; // not a number
+var x = "dfsdf"/34;
+// console.log(x);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+var d = 3n ** 123n;
+
+var e = undefined;
+var f = null;
+
+// Structural
+var g = ["a", "b", "c"];
+var h = { name: "keshab", age: 12 };
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
 
 
 console.log(num)
