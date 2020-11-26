07486474928dc166760cb0602bb35e1db7605087
Hasana Shrestha <i157884@d2hs.com>
2020-11-26 10:00:14 +0545

    startingFunction

---
+++ Function_methods.js
@@ -0,0 +1,22 @@
+function square(number)
+{
+   let squared = number * number;
+   return squared;
+}
+let s = square(123);
+console.log(s);
+
+//function without name anonymous fucntion
+
+
+var f1 = function square(number)
+{
+   let squared = number * number;
+   return squared;
+}
+let s = square(123);
+console.log(s);
+
+//Arrow function
+
+
