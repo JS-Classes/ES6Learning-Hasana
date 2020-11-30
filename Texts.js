59fd03c746608986a80680f2ca772cbf3bfe7fe3
Hasana Shrestha <i157884@d2hs.com>
2020-11-30 12:59:19 +0545

    Learned_Texts

---
+++ Texts.js
@@ -0,0 +1,24 @@
+
+//let hackathon = 'Hackathon 2020' + 12;
+let x  = "X";
+let y = "Y";
+let isMale = true;
+
+//String literal
+let name = `Cotiviti` ${12+34+56}
+console.log(name);
+
+//Regular expression
+let x = "23 889 bcc c2333 5656 564";
+let re1 = /\d/;
+let re2 = /[0-9]/;
+let re3 = new RegExp('[0-9]');
+
+let r = x.match(re1);
+console.log(r);
+
+
+
+
+
+
